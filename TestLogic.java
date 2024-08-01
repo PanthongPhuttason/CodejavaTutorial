19c74056cab461032a2010ce4130a8aebb8701d3
Panrthong_github.com <152240564+PanthongPhuttason@users.noreply.github.com>
2024-08-01 09:12:34 +0700

    Create TestLogic.java

---
+++ TestLogic.java
@@ -0,0 +1,26 @@
+public class TestLogic {
+    public static void main(String[] args) {
+        /*
+         > มากกว่า
+         < น้อยกว่า
+         == เท่ากับ
+         >= มากกว่าเท่ากับ
+         <= น้อยกว่าเท่ากับ
+         != ไม่เท่ากับ
+         && and และ
+         || or หรือ
+         ! not ไม่
+         */
+        int x = 6, y = 9;
+        System.out.println(x>y);
+        System.out.println(x<y);
+        System.out.println(x>=y);
+        System.out.println(x<=y);
+        System.out.println(x==y);
+        System.out.println(x!=6);
+        System.out.println((x < y) && (y > 3));
+        System.out.println((x == 5) || (y > 2));
+        System.out.println(!(y > x));
+        
+    }
+}
