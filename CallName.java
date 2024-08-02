e638502ccdf5ac91ba9d3d5677e482faf4f80680
Panrthong_github.com <152240564+PanthongPhuttason@users.noreply.github.com>
2024-08-02 10:10:24 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,10 @@
+public class CallName {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            Loopname();
+        }
+    }
+    public static void Loopname() {
+        System.out.println("Panthong Phuttason");
+    }
+}
